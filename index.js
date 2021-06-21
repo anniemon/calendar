46305d93bfeccab0c09f82f95ee51bb0edcb38d8
anniemon <seojung19@gmail.com>
2021-06-21 22:55:23 +0900

    start: calendar project

---
+++ index.js
@@ -0,0 +1,14 @@
+const now = new Date();
+// const currentDate = now.getDate();
+// const currentMonth = now.getMonth();
+// const currentDay = now.getDay();
+// const currentYear = now.getFullYear();
+
+// const showDay = document.querySelector(".show-day");
+// const showDate = document.querySelector(".show-date");
+// const showMonthYear = document.querySelector(".show-month-year");
+
+console.dir(now)
+// showDay.textContent = currentDay;
+// showMonthYear.textContent = currentMonth + currentYear;
+// showDate.textContent = currentDate;
