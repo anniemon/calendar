043373d7a9100b739f093280506f3c64e59d60fe
anniemon <seojung19@gmail.com>
2021-06-25 00:41:02 +0900

    add: current Month full date

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const now = new Date();
 const showDay = document.querySelector(".show-day");
 const showDate = document.querySelector(".show-date");
 const showMonthYear = document.querySelector(".show-month-year");
-const calendarBody = document.querySelector(".calendar-body");
+const calendarBody = document.querySelector("tbody");
+const calendarBody_Rows = calendarBody.children;
 
 const DAY_LIST = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT',];
 const MONTH_LIST = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JULY', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
 
 const currentDate = now.getDate();
-const currentMonth = MONTH_LIST[now.getMonth()];
+const currentMonth = now.getMonth();
 const currentDay = DAY_LIST[now.getDay()];
 const currentYear = now.getFullYear();
 
@@ -17,46 +18,52 @@ const currentYear = now.getFullYear();
 function printCurrentDate() {
     showDay.textContent = currentDay;
     showDate.textContent = currentDate;
-    showMonthYear.textContent = `${currentMonth} ${currentYear}`;
+    showMonthYear.textContent = `${MONTH_LIST[now.getMonth()]} ${currentYear}`;
 }
 
-function printDay() {
-    calendarBody.textContent = DAY_LIST[0]
-    for (let i = 1; i < DAY_LIST.length; i++) {
-        calendarBody.textContent += ` ${DAY_LIST[i]}`;
+
+function printCurrentDatesOfMonth() {
+    let dateNumber = 2;
+    // TODO: 현재 월의 1일이 무슨 요일인지 판별하고, 해당 요일 라벨링에 1일 표기하기
+    let firstDay = now.getDay() + 1 - currentDate % 7;
+    console.log(firstDay)
+    for(let row = 0; row < calendarBody_Rows.length; row++){
+        for (let col = 0; col < 7; col++) {
+            calendarBody_Rows[0].children[firstDay].textContent = 1;
+            if(row === 0 && col > firstDay) {
+                calendarBody_Rows[row].children[col].textContent = dateNumber;
+                dateNumber++;
+            } else if( row > 0) {
+                calendarBody_Rows[row].children[col].textContent = dateNumber;
+                dateNumber++;
+                if(dateNumber > calculateDateNumbers(currentMonth)) {
+                    break;
+                }
+            }
+        }
     }
 }
-// TODO: switch문으로 달력 출력하기~!~!
 
-function printCalendarBody() {
-    let dateNumber;
-
+function calculateDateNumbers(month) {
     switch (month) {
-        case MONTH_LIST[0]: case MONTH_LIST[2]: case MONTH_LIST[4]:
-        case MONTH_LIST[6]: case MONTH_LIST[7]: case MONTH_LIST[9]: case MONTH_LIST[11]:
-            dateNumber = 31;
-            break;
-        case MONTH_LIST[3]: case MONTH_LIST[5]:
-        case MONTH_LIST[8]: case MONTH_LIST[10]:
-            dateNumber = 30;
-            break;
-        case MONTH_LIST[1]:
-            dateNumber = 28;
+        case 0: case 2: case 4:
+        case 6: case 7: case 9: case 1:
+        dateNumber = 31;
+        break;
+        case 3: case 5:
+        case 8: case 1:
+        dateNumber = 30;
+        break;
+        case 1:
+        dateNumber = 28;
+        break;
     }
-
-    for (let i = 1; i <= dateNumber; i++) {
-        )
-    }
+    return dateNumber;
 }
-
-
-
-
-
 
 function init() {
     printCurrentDate();
-    printDay();
+    printCurrentDatesOfMonth();
 }
 
 init();
