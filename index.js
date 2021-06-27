e2fc9b9f6f0c3fd573b28947637b3d05af1c7c71
anniemon <seojung19@gmail.com>
2021-06-27 23:26:00 +0900

    add: button content, printNextCalendarBody function, removeCurrentCalendarBody function fix: firstDay calculation, printCalendar functions reusably seperate: highlightToday function

--- index.js
+++ index.js
@@ -5,48 +5,50 @@ const showDate = document.querySelector(".show-date");
 const showMonthYear = document.querySelector(".show-month-year");
 const calendarBody = document.querySelector("tbody");
 const calendarBody_Rows = calendarBody.children;
+const calendarBtns = document.querySelectorAll("button");
 
 const DAY_LIST = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT',];
 const MONTH_LIST = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JULY', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
 
-const currentDate = now.getDate();
-const currentMonth = now.getMonth();
-const currentDay = DAY_LIST[now.getDay()];
-const currentYear = now.getFullYear();
+let currentDate = now.getDate();
+let currentMonth = now.getMonth();
+let currentDay = DAY_LIST[now.getDay()];
+let currentYear = now.getFullYear();
 
 
-function printCurrentDate() {
-    showDay.textContent = currentDay;
-    showDate.textContent = currentDate;
-    showMonthYear.textContent = `${MONTH_LIST[now.getMonth()]} ${currentYear}`;
+function printSeperateDate(day, date, month, year) {
+    showDay.textContent = day;
+    showDate.textContent = date;
+    showMonthYear.textContent = `${month} ${year}`;
 }
 
-
-function printCurrentDatesOfMonth() {
+function printCalendarBody(currentYear, currentMonth) {
     let dateNumber = 2;
-    let firstDay = now.getDay() + 1 - currentDate % 7 
-    firstDay < 0 ? firstDay += 7 : firstDay;
+    let firstDay = new Date(currentYear, currentMonth).getDay();
     for(let row = 0; row < calendarBody_Rows.length; row++){
         for (let col = 0; col < 7; col++) {
             calendarBody_Rows[0].children[firstDay].textContent = 1;
             if(row === 0 && col > firstDay) {
                 calendarBody_Rows[row].children[col].textContent = dateNumber;
                 dateNumber++;
-                if(calendarBody_Rows[row].children[col].textContent === String(currentDate)){
-                    calendarBody_Rows[row].children[col].classList.add("today");
-                }
             } else if( row > 0) {
                 calendarBody_Rows[row].children[col].textContent = dateNumber;
                 dateNumber++;
                 if(dateNumber > calculateDateNumbers(currentMonth)) {
                     break;
                 }
-                if(calendarBody_Rows[row].children[col].textContent === String(currentDate)){
-                    calendarBody_Rows[row].children[col].classList.add("today");
-                }
             }
         }
     }
+}
+
+function highlightToday (currentDate) {
+    // if(calendarBody_Rows[row].children[col].textContent === String(currentDate)){
+    //     calendarBody_Rows[row].children[col].classList.add("today");
+    // }
+    // if(calendarBody_Rows[row].children[col].textContent === String(currentDate)){
+    //     calendarBody_Rows[row].children[col].classList.add("today");
+    // }
 }
 
 function calculateDateNumbers(month) {
@@ -66,9 +68,31 @@ function calculateDateNumbers(month) {
     return dateNumber;
 }
 
+function printNextCalenderBody() {
+    printCalendarBody(currentYear, currentMonth++);
+}
+
+function removecurrentCalenderBody() {
+    for(let i = 0; i < calendarBody_Rows.length; i ++) {
+        for(let j = 0; j < 7; j++) {
+            calendarBody_Rows[i].children[j].textContent = '';
+        }
+    }
+}
+
+function handleClick(e) {
+    if(e.target.classList[0] === 'button-next') {
+        removecurrentCalenderBody();
+        printNextCalenderBody();
+    } else if(e.target.classList[0] === 'button-prev') {
+        
+    }
+}
+
 function init() {
-    printCurrentDate();
-    printCurrentDatesOfMonth();
+    printSeperateDate(currentDay, currentDate, MONTH_LIST[now.getMonth()], currentYear);
+    printCalendarBody(currentYear, currentMonth);
+    calendarBtns.forEach((btn) => btn.addEventListener("click", handleClick));
 }
 
 init();
